b492f48d9612a1bb8e19bcdbc169e627f1e9c524
aidansean <aidan.randleconde@gmail.com>
2015-05-14 22:02:24 +0200

    Fixed typos

--- project.py
+++ project.py
@@ -7,6 +7,6 @@ p.preview_image_ = image_object('http://placekitten.com.s3.amazonaws.com/homepag
 p.folder_name = 'greasemonkey'
 p.github_repo_name = 'i'
 p.mathjax = False
-p.links.append(link_object(p.domain, 'i/', 'indigo page'))
-p.introduction = 'One of the projects I wrote was an indico uri shortening service, called <em>i</em> (or indigo, depending on how ugly "<em>i</em>" looks in context) but I wanted to go a step further.  So I write a greasemonkey script so that I could get to indico pages without even loading the main page.  This takes up very few pixels on the screen, making it much faster to get to the meeting pages.'
+p.links.append(link_object(p.domain, 'indigo/', 'indigo page'))
+p.introduction = 'One of the projects I wrote was an indico uri shortening service, called <em><a hhref="http://aidansean.com/projects/?p=191">i</a></em> (or indigo, depending on how ugly "<em>i</em>" looks in context) but I wanted to go a step further.  So I write a greasemonkey script so that I could get to indico pages without even loading the main page.  This takes up very few pixels on the screen, making it much faster to get to the meeting pages.'
 p.overview = '''This is a rather simple script.  It simply redirects (in a new tab) to the <em>i</em> page, which redirects to indico.'''
